5bcfd4ba1c6c061bd0b1b566f7e02246c9b4b641
1sunJ <won9619v@naver.com>
2025-02-15 21:15:06 +0900

    Initial commit

---
+++ src/main/java/com/_sunj/concurrency_demo/ConcurrencyDemoApplication.java
@@ -0,0 +1,13 @@
+package com._sunj.concurrency_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_sunj/concurrency_demo/ConcurrencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._sunj.concurrency_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
